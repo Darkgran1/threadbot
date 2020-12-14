b2c094e9c7fe0dab4d0b9942049e5a5bc9d68e67
Darkgran1 <56434620+Darkgran1@users.noreply.github.com>
2020-12-14 11:46:20 +0000

    Removed secrets
    Revert "Removing mongo DB features and Teams"
    
    This reverts commit 80b6791a3a1342e556edba82facedddcd1566c93.
    
    Removing mongo DB features and Teams

--- src/init/mongoose.ts
+++ src/init/mongoose.ts
@@ -3,6 +3,9 @@ import * as mongoose from 'mongoose'
 export const mongodbURI = process.env.MONGODB_URI!
 if (!mongodbURI) throw new Error('[ENV] Missing environment variable: MONGODB_URI')
 
-mongoose.connect(mongodbURI, { useNewUrlParser: true })
+mongoose.connect(
+  mongodbURI,
+  { useNewUrlParser: true },
+)
 
 export default mongoose
